bc11f994188f70039c1f10ad6b2d0080922bbe74
noormohammad20 <mdsaim204@gmail.com>
2022-05-28 13:42:35 +0600

    Basic types number string boolean interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let student: string = 'jolly'
+let age: number = 33
+let isSmart: boolean = true
+
+let students: string[] = ['joe', 'biden', 'harris']
+let fees: number[] = [33, 35, 64, 33]
+
+
+interface Person {
+  name: string,
+  job?: string,
+  age: number
+}
+
+const person: Person = {
+  name: 'bil',
+  age: 35
+}
+
 
 function App() {
   return (
---
+++ src/vanila/vanila.js
@@ -0,0 +1,8 @@
+let user = 'Rovert'
+user = 55
+user = false
+user = { id: 1, age: 3, address: 'kolimuddin road' }
+user = [3, 5, 23, 53]
+user = ['rock', 'jhon', 'marsh']
+user = () => 55
+console.log(user)
